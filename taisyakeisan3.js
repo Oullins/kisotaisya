d3fa2ee90fcf4c859b499e5b70a81ffc02d7ac11
Oullins <nichibotxu@yahoo.fr>
2021-09-04 20:36:07 +0900

    Update taisyakeisan3.js

--- taisyakeisan3.js
+++ taisyakeisan3.js
@@ -21,10 +21,12 @@ var value6=parseFloat(document.getElementById("mets5").value);
 	var result2 = Math.round(24*value2**0.725 * value1**0.425 *0.007184 *0.8* 4.88*10**(-8)* ((273+value3)**4-(273+value4)**4))
 	var result3 = Math.round((value1*15+200*(value3-36.8))/1000/24*580*24)
 	var result4 = Math.round((value6-1)*value1*value5*1.05)
+	var result5 = Math.round(result2+result3+result4)
 	document.querySelector('.result1').innerHTML = result1;
 	document.querySelector('.result2').innerHTML = result2;
 	document.querySelector('.result3').innerHTML = result3;
 	document.querySelector('.result4').innerHTML = result4;
+	document.querySelector('.result5').innerHTML = result5;
 }
 
 function formSwitch(){
